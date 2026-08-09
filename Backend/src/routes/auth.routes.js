613f5e60cba18d5d5f19dcef24c3db7c7afac324
AnantKumarSingh26 <anantsingh.code@gmail.com>
2026-08-09 22:07:28 +0530

    verification of email✔,login ✔ ️get-me ✔ ️

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,9 +1,19 @@
-import {Router} from "express"
-import {register} from '../controller/auth.controller.js'
-import { registerValidator } from "../validators/auth.validator.js"
+import { Router } from "express"
+import { getMe, login, register, verifyEmail } from '../controller/auth.controller.js'
+import { loginValidator, registerValidator } from "../validators/auth.validator.js"
+import { authUser } from "../middleware/auth.middleware.js"
 
 const authRouter = Router()
 
-authRouter.post("/register",registerValidator,register)
+// @route get
+
+authRouter.post("/register", registerValidator, register)
+
+authRouter.post("/login", loginValidator, login)
+
+authRouter.get("/get-me", authUser, getMe)
+
+authRouter.get("/verify-email", verifyEmail)
+
 
 export default authRouter

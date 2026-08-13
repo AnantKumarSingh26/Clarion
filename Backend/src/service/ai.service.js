8b975c22324184fc1e99efb062e1f871206e74bc
AnantKumarSingh26 <anantsingh.code@gmail.com>
2026-08-13 16:20:48 +0530

    Frontend begins

--- Backend/src/service/ai.service.js
+++ Backend/src/service/ai.service.js
@@ -1,16 +1,20 @@
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 
 const model = new ChatGoogleGenerativeAI({
-  model: "gemini-3.6-flash",
+//   model: "gemini-3.6-flash",
+  model: "gemini-3.5-flash-lite",
   apiKey: process.env.GOOGLE_API_KEY
 });
 export async function testAi() {
 
-    try{const question = "What is the capital of India"
+    try{const question = "What is your name"
         console.log(question)
         const response = await model.invoke(question);
         console.log(response.content)
     }catch(err){
         console.error("AI Error:", err.message)
     }
+}
+export async function sendEmail(to, subject,html,text) {
+    
 }

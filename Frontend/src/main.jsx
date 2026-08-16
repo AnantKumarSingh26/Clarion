3b77d9710fe9edcc234d70a62af8be7b8d2b558d
AnantKumarSingh26 <anantsingh.code@gmail.com>
2026-08-16 22:32:50 +0530

    Add Dashboard component with chat interface and styling

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,11 +5,13 @@ import App from './app/App.jsx'
 import { store } from './app/app.store.js'
 import { Provider } from 'react-redux'
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
 
       <App />
+      {/* <Dashboard/> */}
 
     </Provider>
   </StrictMode>,

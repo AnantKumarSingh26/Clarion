dac0e5c4ceb27cc9d9a8860120cbbc1d1a8b34fb
AnantKumarSingh26 <anantsingh.code@gmail.com>
2026-08-22 19:24:29 +0530

    migrate email service to brevo smtp

--- Backend/src/service/mail.service.js
+++ Backend/src/service/mail.service.js
@@ -1,18 +1,26 @@
 import nodemailer from 'nodemailer'
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-        user: process.env.GOOGLE_USER,
-        pass: process.env.GOOGLE_APP_PASSWORD,
-    },
+    // service: 'gmail',
+    // host: 'smtp.gmail.com',
+    // port: 465,
+    // secure: true,
+    // auth: {
+    //     user: process.env.GOOGLE_USER,
+    //     pass: process.env.GOOGLE_APP_PASSWORD,
+    // },
+    host:process.env.BREVO_SMTP_HOST || 'smtp-relay.brevo.com',
+    port: Number(process.env.BREVO_SMTP_PORT ) || 587,
+    secure:false,
+    auth:{
+        user: process.env.BREVO_SMTP_USER,
+        pass: process.env.BREVO_API_KEY
+    }
+
 })
 transporter.verify()
     .then(() => {
-        console.log('Email transporter is ready to send emails')
+        console.log('Email transporter is ready to send emails with Brevo')
     })
     .catch((err) => {
         console.error('Email transporter verification failed', err)
@@ -20,7 +28,7 @@ transporter.verify()
 
 export async function sendEmail(to, subject, html, text) {
     const mailOptions = {
-        from: process.env.GOOGLE_USER,
+        from:` Clarion <${process.env.GOOGLE_USER || process.env.BREVO_SMTP_USER}`,
          to,
         subject,
         html,
